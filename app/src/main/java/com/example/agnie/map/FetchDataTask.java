051650f03e9456899b149b804c0250559ed35739
Oleksii Kyrylchuk <olety7@gmail.com>
2017-06-20 13:19:10 +0200

    Updated everything
    Changes:
    - Added getting events based on location
    - Added custom infowindows
    - General code cleanup (as much as possible without breaking stuff)
    - [SERVER] Improved performance
    
    TODO:
    - Make the update_events async? OR show a splash screen
    - Add a slider for the searchRange variable

---
+++ app/src/main/java/com/example/agnie/map/FetchDataTask.java
@@ -0,0 +1,83 @@
+package com.example.agnie.map;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by olety on 5/30/17.
+ */
+
+public class FetchDataTask extends AsyncTask<String, Void, JSONArray> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected JSONArray doInBackground(String... params) {
+        // Init the connection
+        URL urlCould;
+        HttpURLConnection connection;
+        InputStream inputStream = null;
+        try {
+            String url = params[0];
+            urlCould = new URL(url);
+            connection = (HttpURLConnection) urlCould.openConnection();
+            connection.setConnectTimeout(3000000);
+            connection.setReadTimeout(3000000);
+            connection.setRequestMethod("GET");
+            System.out.println(urlCould);
+            connection.connect();
+
+            inputStream = connection.getInputStream();
+
+        } catch (MalformedURLException MEx){
+
+        } catch (IOException IOEx){
+            IOEx.printStackTrace();
+            Log.e("Utils", "HTTP failed to fetch data");
+            return null;
+        }
+        // Get the res. string
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // Convert the res. string to json
+        JSONArray json = null;
+        try {
+            json = new JSONArray(sb.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    protected void onPostExecute(String string) {
+    }
+}
